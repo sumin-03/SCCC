8744f0c2644e80395e5a885ff23f88f0e2b219e3
sumin-03 <doorcinema0000@naver.com>
2025-03-22 03:37:05 +0900

    .

---
+++ week01/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    cout << "hello world\n";
+
+    return 0;
+}
